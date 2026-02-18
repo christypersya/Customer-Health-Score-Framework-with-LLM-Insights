e9b12c17d0b3e5a1cd4e3f6b6284b0c623883061
christypersya <39495319+christypersya@users.noreply.github.com>
2026-02-18 12:04:45 -0700

    Add explainability module implementing SHAP summary plot

---
+++ src/explainability.py
@@ -0,0 +1,7 @@
+import shap
+import matplotlib.pyplot as plt
+
+def shap_summary(model, X):
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X)
+    shap.summary_plot(shap_values, X, show=True)
